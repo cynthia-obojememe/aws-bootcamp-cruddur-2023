9d90c8760b44ff4c5e7f4e5b0058ffcc8f5d64f2
cynthia Obojememe <cynthia.obojememe@gmail.com>
2023-07-20 13:03:23 +0000

    implement postgres for codespaces

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set up PostgreSQL
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+sudo apt-get update
+sudo apt-get install -y postgresql-client-13 libpq-dev
+sudo apt-get install -y apt-utils
+
+# Export environment variable GITPOD_IP
+export GITPOD_IP=$(curl ifconfig.me)
+
+# Run your desired command
+source "$THEIA_WORKSPACE_ROOT/backend-flask/bin/db-update-sg-rule"
