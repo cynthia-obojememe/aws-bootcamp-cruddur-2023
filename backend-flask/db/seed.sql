674176f79aef05cdee01e14727cef9e10918586f
mima35 obo <cynthia562013@gmail.com>
2023-07-27 16:20:37 +0000

    setup some parts of message.py / seed backend-flask

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,13 +2,13 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Cynthia obojememe','cynthialearning3@gmail.com' , 'yee' ,'MOCK'),
-  ('Andrew Bayko','cynthia562013@gmail.com' , 'bayko' ,'MOCK');
+  ('cynthia','cynthia562013@gmail.com' , 'testt' ,'MOCK');
   
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'Firdous' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'yee' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
